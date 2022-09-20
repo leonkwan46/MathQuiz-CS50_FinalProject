3e0af4c675ca23caf259e89c80d7a3928a9951ac
leonkwan46 <66780058+leonkwan46@users.noreply.github.com>
2022-09-20 18:53:46 +0100

    added comment

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# DB & MySQL Connection
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""

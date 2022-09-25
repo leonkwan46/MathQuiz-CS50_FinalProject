93ef469c04b70bb12151b0c54a36ef9282feb9dd
leonkwan46 <66780058+leonkwan46@users.noreply.github.com>
2022-09-25 22:14:11 +0100

    Login & Register Page all working successfully

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, make_response, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from flask_mysqldb import MySQL
 from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import g, request, redirect, url_for
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
+CORS(app)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 if __name__ == '__main__':
@@ -20,9 +24,8 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "quiz"
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
-db = mysql.connection.cursor()
-
 
 def login_required(f):
     @wraps(f)
@@ -32,63 +35,67 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route("/")
-@login_required
+@app.route("/", methods=["GET", "POST"])
+# @login_required
 def home():
+    db = mysql.connection.cursor()
+    # user_id = session["user_id"]
     if request.method == "GET":
-        user_id = session["user_id"]
-        score_data = db.execute("SELECT score_easy, score_medium, score_hard FROM users WHERE user_id=? ORDER BY ASC", user_id)
-        return render_template(".js", score_data=score_data)
-
+        db.execute("SELECT score_easy, score_medium, score_hard FROM users WHERE userID=(%s)",'1')
+        data = db.fetchall()
+        return make_response(jsonify(data))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     session.clear()
     if request.method == "GET":
-        return render_template("login.html")
-    
+        return render_template("test2.html")
+
     else:
         if not request.form.get("username"):
-            return ERROR 
+            return make_response(jsonify({'errorMessage': 'Login failed'}), 401)
         if not request.form.get("password"):
-            return ERROR
+            return make_response(jsonify({'errorMessage': 'Login failed'}), 401)
 
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username").strip())
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password").strip()):
-            return ERROR
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
+        
+        db = mysql.connection.cursor()
+        db.execute("SELECT * FROM users WHERE username LIKE %s", [username])
+        rows = db.fetchall()
+        if len(rows) != 1 or rows[0]["password"] != password:
+            return make_response(jsonify({'errorMessage': 'Login failed 2'}), 401)
 
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["userID"]
         return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
 
     session.clear()
-
     if request.method == "GET":
-        return render_template(".js")
+        return render_template("test3.html")
 
     else:
+        con = mysql.connection
+        db = con.cursor()
+    
         username = request.form.get("username").strip()
         password = request.form.get("password").strip()
         confirm = request.form.get("confirmation").strip()
 
         if not username:
-            return ERROR
+            return make_response(jsonify({'errorMessage': 'Register failed'}), 401)
         if not password:
-            return ERROR
+            return make_response(jsonify({'errorMessage': 'Register failed'}), 401)
         if not confirm:
-            return ERROR
+            return make_response(jsonify({'errorMessage': 'Register failed'}), 401)
         if password != confirm:
-            return ERROR
+            return make_response(jsonify({'errorMessage': 'Password not the same'}), 401)
 
-        else:
-            hash = generate_password_hash(password)
-            try:
-                new_user = db.execute("INSERT INTO users(username, hash, password) VALUES (?,?,?)", username, hash, password)
-            except:
-                return ERROR
-
-            session["user_id"] = new_user
-            return redirect("/")
+        db.execute("INSERT INTO users(username, password) VALUES (%s,%s)", (username, password))
+        con.commit()
+        new_user = db.fetchall()
+        session["user_id"] = new_user
+        return redirect("/")

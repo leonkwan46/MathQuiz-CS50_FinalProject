a5face3c7c23f0b0b67a219430d04bb9af5fcb12
leonkwan46 <66780058+leonkwan46@users.noreply.github.com>
2022-11-29 15:54:46 +0000

    Revised

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\leonk\Desktop\cs50fp\venv
+set VIRTUAL_ENV=C:\Users\leonk\Desktop\Coding Projects\MathWebsite\cs50fp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
